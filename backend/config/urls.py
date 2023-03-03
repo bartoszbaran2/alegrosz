29c6ffb41875a136d14a70292eb9c8a3978a94ce
Bartosz Baran <bartosz.baran192@gmail.com>
2023-03-03 22:10:05 +0100

    change admin endpoint name

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin123/', admin.site.urls),
     path('api/v1/', include('products.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
