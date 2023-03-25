38d0481fb87245f7f695a384147b95a91e0cbfad
Bartosz Baran <bartosz.baran192@gmail.com>
2023-03-25 10:21:51 +0100

    add product model to admin and set media dir

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+class ProductAdminConfig(admin.ModelAdmin):
+    """Class **ProductAdminConfig** displays products list
+    in admin panel based on :class`product.models.Product` model."""
+
+    list_display = ("name", "price", "stock_count", "popularity", "rank", "sales_count")
+    search_fields = ("name",)
+    list_editable = ("price", "popularity", "rank", "stock_count")
+    list_display_links = ("name",)
+    save_on_top = True
+    list_filter = ("created_at",)
+    list_per_page = 50
+
+
+admin.site.register(Product, ProductAdminConfig)
