f077c8b4271cb5b7a5259ebbad34c1478e7d1f47
Bartosz Baran <bartosz.baran192@gmail.com>
2023-03-27 19:37:33 +0200

    add custom admin  panel title

--- config/urls.py
+++ config/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Alegrosz Co."
+admin.site.index_title = "Alegrosz"
+admin.site.site_title = "Alegrosz"
