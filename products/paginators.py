991736783dacebee758930c4744b2ba9509574d8
Bartosz Baran <bartosz.baran192@gmail.com>
2023-04-15 20:25:36 +0200

    custom paginator

--- products/paginators.py
+++ products/paginators.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPaginator(PageNumberPagination):
-    page_size = 20
+    page_size = 6
     page_query_param = "page"
     max_page_size = 100
