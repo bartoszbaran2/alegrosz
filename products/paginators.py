21b26bf9f39125fbf910634ad087ea300a0f9fd2
Bartosz Baran <bartosz.baran192@gmail.com>
2023-04-24 18:07:10 +0200

    set pagination

--- products/paginators.py
+++ products/paginators.py
@@ -1,7 +1,7 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 
-class CustomPaginator(PageNumberPagination):
-    page_size = 6
+class CustomPaginator(LimitOffsetPagination):
+    page_size = 20
     page_query_param = "page"
     max_page_size = 100
