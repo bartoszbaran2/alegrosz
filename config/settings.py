f3c2ac81810a95b615bf36359fd5f0e9542b881f
Bartosz Baran <bartosz.baran192@gmail.com>
2023-04-27 12:16:57 +0200

    redis instalation with basic config

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,13 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
